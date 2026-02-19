8d97aae29aedd0915d94e66750ed18c2c688bac4
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-02-19 19:16:00 +0530

    Update directory.js

--- assets/js/directory.js
+++ assets/js/directory.js
@@ -163,7 +163,7 @@ const silenVaultTools = [
         iconColors: "bg-purple-500/10 text-purple-400 group-hover:bg-purple-500",
         // Vibration/Phone Icon
         iconPath: `<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z" />`
-    }
+    },
 
     // --- CREATOR TOOLS ---
     {
