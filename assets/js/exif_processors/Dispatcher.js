5202c5532774210f33c42144cb1db5e71fdb1c88
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-02-20 23:55:50 +0530

    Update Dispatcher.js

--- assets/js/exif_processors/Dispatcher.js
+++ assets/js/exif_processors/Dispatcher.js
@@ -2,18 +2,28 @@ import { WebpProcessor } from './WebpProcessor.js';
 import { JpegProcessor } from './JpegProcessor.js';
 import { PngProcessor } from './PngProcessor.js';
 import { RawProcessor } from './RawProcessor.js';
-import { CanvasProcessor } from './CanvasProcessor.js';
 
 export class ProcessorFactory {
     static create(file, ext) {
         const extension = ext.toUpperCase();
         
         switch(extension) {
-            case 'WEBP': return new WebpProcessor(file);
-            case 'PNG': return new PngProcessor(file);
-            case 'DNG': case 'CR2': case 'NEF': case 'ARW': return new RawProcessor(file);
-            case 'JPG': case 'JPEG': case 'JFIF': return new JpegProcessor(file);
-            default: return new CanvasProcessor(file, ext, 'image/jpeg'); 
+            case 'WEBP':
+                return new WebpProcessor(file);
+            case 'PNG':
+                return new PngProcessor(file);
+            case 'DNG':
+            case 'CR2':
+            case 'NEF':
+            case 'ARW':
+                return new RawProcessor(file);
+            case 'JPG':
+            case 'JPEG':
+            case 'JFIF':
+                return new JpegProcessor(file);
+            default:
+                console.warn(`No dedicated processor for ${extension}. Using generic JPEG Splicer.`);
+                return new JpegProcessor(file); 
         }
     }
 }
