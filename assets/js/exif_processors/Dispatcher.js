e12dc1ea6e4e91ebe47265d86c56e975cc0d5a16
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-02-21 02:43:55 +0530

    Update Dispatcher.js

--- assets/js/exif_processors/Dispatcher.js
+++ assets/js/exif_processors/Dispatcher.js
@@ -9,23 +9,17 @@ export class ProcessorFactory {
         const extension = (ext || '').toUpperCase();
         
         switch(extension) {
-            case 'WEBP': 
-                return new WebpProcessor(file, extension);
-            case 'PNG': 
-                return new PngProcessor(file, extension);
+            case 'WEBP': return new WebpProcessor(file, extension);
+            case 'PNG': return new PngProcessor(file, extension);
             case 'DNG': 
             case 'CR2': 
             case 'NEF': 
             case 'ARW':
-                return isAdvancedMode 
-                    ? new RawProcessor(file, extension) 
-                    : new CanvasProcessor(file, extension, 'image/jpeg');
+                return isAdvancedMode ? new RawProcessor(file, extension) : new CanvasProcessor(file, extension, 'image/jpeg');
             case 'JPG': 
             case 'JPEG': 
-            case 'JFIF': 
-                return new JpegProcessor(file, extension);
-            default:
-                return new CanvasProcessor(file, extension, 'image/jpeg'); 
+            case 'JFIF': return new JpegProcessor(file, extension);
+            default: return new CanvasProcessor(file, extension, 'image/jpeg'); 
         }
     }
 }
